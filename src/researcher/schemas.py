59249aff44ff6d2353625243c26fda12c1e5336f
Brendon <b10815044@gapps.ntust.edut.tw>
2024-12-08 19:16:48 +0000

    update scheme

--- src/researcher/schemas.py
+++ src/researcher/schemas.py
@@ -2,13 +2,6 @@ from __future__ import annotations
 from typing import Optional
 from pydantic import BaseModel
 
-# currently not used
-class ResearchPaper(BaseModel):
-    paper_title: Optional[str] = None
-    paper_link: Optional[str] = None
-    demo_video_link: Optional[str] = None
-    project_website: Optional[str] = None
-
 class ResearchProfile(BaseModel):
     image_url: Optional[str] = None
     google_scholar_link: Optional[str] = None
